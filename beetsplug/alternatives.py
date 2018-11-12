e34875085b4d044dde17041c73f27bceeb44cd74
wisp3rwind <17089248+wisp3rwind@users.noreply.github.com>
2018-11-12 23:53:43 +0100

    Support format aliases from the convert plugin

--- beetsplug/alternatives.py
+++ beetsplug/alternatives.py
@@ -254,7 +254,8 @@ class ExternalConvert(External):
         convert_plugin = convert.ConvertPlugin()
         self._encode = convert_plugin.encode
         self._embed = convert_plugin.config['embed'].get(bool)
-        self.formats = [f.lower() for f in formats]
+        formats = [f.lower() for f in formats]
+        self.formats = [convert.ALIASES.get(f, f) for f in formats]
         self.convert_cmd, self.ext = convert.get_format(self.formats[0])
 
     def converter(self):
