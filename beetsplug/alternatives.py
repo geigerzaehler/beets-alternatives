c9df2a76c497e4d49f814de8f6ad2fc642f031a9
Thomas Scholtes <geigerzaehler@axiom.fm>
2024-04-01 20:07:53 +0200

    Use pathlib in tests

--- beetsplug/alternatives.py
+++ beetsplug/alternatives.py
@@ -10,7 +10,6 @@
 # The above copyright notice and this permission notice shall be included in
 # all copies or substantial portions of the Software.
 
-
 import argparse
 import os.path
 import threading
